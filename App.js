9c5bac76c11d406c71bfc8daeb62c252c6823f06
yashas8197 <yashasjr7@gmail.com>
2023-09-02 20:37:04 +0530

    function component

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <div>
-    <h1>sample run</h1>
-  </div>
-);
+const Title = () => {
+  return <h1>I am yashas</h1>;
+};
+
+const Title1 = () => {
+  return <h2>I am learning react</h2>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      {Title1()}
+      <h3>functionl component </h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
