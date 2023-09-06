3ed0bd4adfc8db3e90ebad86fbf35c2e58750154
yashas8197 <yashasjr7@gmail.com>
2023-09-06 22:55:35 +0530

    hard coded data page

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => {
-  return <h1>I am yashas</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/simple-minimalist-food-bag-restaurant-delivery-service-logo-design-vector_493994-1029.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const Title1 = () => {
-  return <h2>I am learning react</h2>;
+const ResturentCard = ({ resName, cuisines, starRating }) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/1/50691/0435a03f4d2017a0a64d90b279c2fa63.jpg"
+      />
+      <h3>{resName}</h3>
+      <h3>{cuisines}</h3>
+      <h3>{starRating}</h3>
+    </div>
+  );
 };
 
-const HeadingComponent = () => {
+const Body = () => {
   return (
     <div>
-      <Title />
-      {Title1()}
-      <h3>functionl component </h3>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturentCard
+          resName="Meghana Foods"
+          cuisines="Biriyani , North Indian , Asian"
+          starRating="4.4"
+        />
+        <ResturentCard resName="KFC" cuisines="Burgers" starRating="4.3" />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
