8722ac71d85e537038f8ac1b9e6f991d732da8a8
yashas8197 <yashasjr7@gmail.com>
2023-10-04 11:27:36 +0530

    added login-logout button

--- src/components/Hearder.js
+++ src/components/Hearder.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../../utils/Constant";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
